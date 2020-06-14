421507a4779f9e9909c2ddc52d22222eb569ea7a
johntremont <piloto2810@gmail.com>
2020-06-13 22:30:01 -0400

    Rendeirng wheather propieties

--- src/app/Weather.js
+++ src/app/Weather.js
@@ -6,8 +6,9 @@ export class Weather {
   }
 
   async getWeather() {
-    const URI = `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&appid=${this.apikey}`;
-    const data = await fetch(URI);
+    const URI = `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&appid=${this.apikey}&units=metric`;
+    const response = await fetch(URI);
+    const data = await response.json();
     return data;
   }
 }
--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,16 @@
 const { Weather } = require('./Weather.js');
-
-const weather = new Weather('London', 'uk');
-
+const { UI } = require('./UI');
 require('./index.css');
 
-function fetchWeather() {
-   weather.getWeather
+
+const ui = new UI();
+const weather = new Weather('Santiago', 'cl');
+
+
+async function fetchWeather() {
+  const data = await weather.getWeather();
+  console.log(data);
+  ui.render(data);
 }
 
 document.addEventListener('DOMContentLoaded', fetchWeather);
