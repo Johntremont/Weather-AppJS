cd229d1b06c09f92867a9619fe5b1800fccfef7d
johntremont <piloto2810@gmail.com>
2020-06-13 22:57:54 -0400

    fix some bugs

--- src/app/index.js
+++ src/app/index.js
@@ -13,4 +13,13 @@ async function fetchWeather() {
   ui.render(data);
 }
 
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    const countryCode = document.getElementById('countryCode').value
+    weather.changeLocation(city, countryCode);
+    fetchWeather();
+    e.preventDefault();
+  
+  });
+
 document.addEventListener('DOMContentLoaded', fetchWeather);
